f43a19ca9a91b543eff213de2c19a4a464439ca3
sujal shresth poddar <99262906+sujal325@users.noreply.github.com>
2022-07-26 11:01:12 +0530

    Add files via upload

---
+++ number line pattern 6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+void my(int a){
+    int i=1;
+    while(i<=a){
+        int z=i;
+        int j=1;
+        while(j<=i){
+            cout<<z<<" ";
+            z+=1;
+            j+=1;
+        }
+        cout<<endl;
+        i+=1;
+    }
+}
+
+int main(){
+    int n;
+    cout<<"how many numbers of line of code do you want to print: ";
+    cin>>n;
+    my(n);
+}
